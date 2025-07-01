504ff3724d6a56319512546a0916509936e0a65f
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-07-01 14:52:51 +0100

    implement cocktail animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
 import gsap from 'gsap'
-import React from 'react'
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth: 767});
      useGSAP(()=> {
         const heroSplit = new SplitText('.title', {type: 'chars, words'});
         const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -41,7 +44,24 @@ const Hero = () => {
             y: -200,
             scale: 1.12,
         }, 0)
-        
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
      },[])
   return (
     <>
@@ -68,6 +88,9 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+        <div className="video absolute inset-0">
+            <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload='auto'></video>
+        </div>
     </>
   )
 }
