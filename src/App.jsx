ddfb380ff635c45295f027c146edfd9d7248266c
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-07-08 13:36:12 +0100

    optimse image loading

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,76 @@ import Art from './components/Art';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 gsap.registerPlugin(ScrollTrigger, SplitText);
+import { useState
+} from 'react';
+import { useEffect } from 'react';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+const assetPromises = [];
+
+const imagesUrl = [
+  '/images/hero-left-leaf.png',
+  '/images/hero-right-leaf.png',
+  '/images/noise.png',
+];
+
+imagesUrl.forEach((src) => {
+  const promise = new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = resolve;
+    img.onerror = reject;
+  }); 
+
+  assetPromises.push(promise);
+})
+
+const videoUrls = [
+  '/images/output.mp4',
+];
+imagesUrl.forEach((src) => {
+  const promise = new Promise((resolve, reject) => {
+    const video = document.createElement('video');
+    video.src = src;
+    video.oncanplaythrough = resolve;
+    video.onerror = reject;
+  }); 
+
+  assetPromises.push(promise);
+})
+
+assetPromises.push(document.fonts.ready);
+
+Promise.all(assetPromises)
+  .then(() => {
+    console.log("All assets loaded!");
+    setLoading(false);
+  })
+  .catch((error) => {
+    console.error("Failed to load one or more assets:", error);
+    setLoading(false);
+  });
+
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Cocktails />
-      <About />
-      <Art />
-      <Menu />
-      <Footer />
-    </main>
+    <>
+    {
+      loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <main>
+          <Navbar />
+          <Hero />
+          <Cocktails />
+          <About />
+          <Art />
+          <Menu />
+          <Footer />
+      </main>
+      )
+    }
+    </>
   )
 }
 
